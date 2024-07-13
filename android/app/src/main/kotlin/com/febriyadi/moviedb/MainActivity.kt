a8aecc7fe7786c6d60beb0f4e7fb4fbf33def7d5
pebbDev <flutterlearning6@gmail.com>
2024-07-13 20:38:06 +0800

    set up project & create splash screen

---
+++ android/app/src/main/kotlin/com/febriyadi/moviedb/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.febriyadi.moviedb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrl = 'https://image.tmdb.org/t/p/w500';
+  static const String imageUrlOriginal = 'https://image.tmdb.org/t/p/original';
+  static const String api_key = 'afe596583b0dbe1fe5a45ce83e1ae9c2';
+}
